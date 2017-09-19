2e2f34de20f56649219f0cd5aad0acfb6584e5b1
kamedashigeru <kamedashigeru@gmail.com>
2017-09-19 22:41:45 +0900

    Create page for tasks/index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def current_user
+    User.find_by(id: session[:user_id]) || User::Logout.new
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,10 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save!
-      render :new
+    user = User.new(user_params)
+    if user.save!
+      session[:user_id] = user.id
+      redirect_to controller: :tasks, action: :index, year: Date.today.year, month: Date.today.month
     else
       render :new
     end
