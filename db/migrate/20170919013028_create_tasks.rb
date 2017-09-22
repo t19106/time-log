06c226d6cbb18687c3e94ce7505e7e798232c78a
shigeru <s.kameda@tsubaiso.jp>
2017-09-22 16:57:12 +0900

    Fix tasks:scheme

--- db/migrate/20170919013028_create_tasks.rb
+++ db/migrate/20170919013028_create_tasks.rb
@@ -2,6 +2,8 @@ class CreateTasks < ActiveRecord::Migration[5.1]
   def change
     create_table :tasks do |t|
       t.datetime :datetime
+      t.time :start_time
+      t.time :end_time
       t.string :title
       t.references :user, foreign_key: true
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,8 @@ ActiveRecord::Schema.define(version: 20170919013028) do
 
   create_table "tasks", force: :cascade do |t|
     t.datetime "datetime"
+    t.time "start_time"
+    t.time "end_time"
     t.string "title"
     t.integer "user_id"
     t.datetime "created_at", null: false
