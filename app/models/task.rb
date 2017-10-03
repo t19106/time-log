0a60b542113c719e14320705e262f62c90a1f824
kamedashigeru <kamedashigeru@gmail.com>
2017-10-03 22:57:05 +0900

    Add validation to model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,24 @@
 class Task < ApplicationRecord
   belongs_to :user
 
+  validates :memo, presence: true
+  validates :starts_at, presence: true
+  validates :ends_at, presence: true
+  validate :ended_after_started?
+  validate :less_than_a_day?
+
+  def ended_after_started?
+    if starts_at >= ends_at
+      errors.add(:ends_at, ': 終了時間は開始時間より前に設定できません。')
+    end
+  end
+
+  def less_than_a_day?
+    unless to_hours < 24
+      errors.add(:ends_at, ': ２４時間を超えるタスクは設定できません。')
+    end
+  end
+
   def to_hours
     ((ends_at - starts_at) / 60 / 60).to_i
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :tasks
   has_secure_password
 
+  validates :mail, uniqueness: true
+
   def login?
     persisted?
   end
