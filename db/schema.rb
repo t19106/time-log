71a670d884a2b1a363f5b727783f6a6a4c21505e
kamedashigeru <kamedashigeru@gmail.com>
2017-10-03 22:06:11 +0900

    Change schema again

--- db/schema.rb
+++ db/schema.rb
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 20170919013028) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "mail"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
