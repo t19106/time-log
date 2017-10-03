e70976dbce23305c87ac0a816be15082321fc2e6
kamedashigeru <kamedashigeru@gmail.com>
2017-10-03 22:04:19 +0900

    Change schema name

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20170919013028) do
   create_table "tasks", force: :cascade do |t|
     t.datetime "starts_at"
     t.datetime "ends_at"
-    t.string "title"
+    t.string "memo"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
