b72364e8d22ccfce9b53e6c02022b6f816848b65
kamedashigeru <kamedashigeru@gmail.com>
2017-10-03 21:09:53 +0900

    Add before_action uri checker

--- app/controllers/tasks/days_controller.rb
+++ app/controllers/tasks/days_controller.rb
@@ -1,8 +1,7 @@
 class Tasks::DaysController < ApplicationController
-  before_action :verify_user
+  before_action :verify_user, :validate_uri
 
   def index
-    @date = Time.zone.local(params[:year], params[:month], params[:day])
     @tasks = Task.where(user: current_user, starts_at: @date.beginning_of_day..@date.end_of_day).or(Task.where(user: current_user, ends_at: @date.beginning_of_day..@date.end_of_day)).map { |task| task.adjust_overnight_range(@date) }.sort_by { |t| t.starts_at }
     @task = Task.new
   end
