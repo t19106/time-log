e33852d7e853c166f503e85a2b5a160d5350a608
kamedashigeru <kamedashigeru@gmail.com>
2017-10-09 23:06:02 +0900

    Add controller tags

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'tags/new'
+
+  get 'tags/create'
+
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get 'logout', to: 'sessions#destroy'
